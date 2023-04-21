9f9d604fb99ee6e2c344688fb693082265bcf403
Abdul Rehman Dar <abdulrehman.dar@invozone.com>
2023-04-21 16:11:41 +0500

    task 1 completed

--- nest_typeorm/src/events/events.service.ts
+++ nest_typeorm/src/events/events.service.ts
@@ -93,7 +93,14 @@ export class EventsService {
 
   @Get('events')
   async getEventsWithWorkshops() {
-    throw new Error('TODO task 1');
+    return await this.eventRepository.find({
+      relations: ['workshops'],
+      order: {
+        workshops: {
+          id: 'ASC',
+        },
+      },
+    });
   }
 
   /* TODO: complete getFutureEventWithWorkshops so that it returns events with workshops, that have not yet started
